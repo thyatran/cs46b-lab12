9186f0ac4b5c031ad6411fd8c244d2f3b58424e9
Thy Tran <thyatran0113@gmail.com>
2023-11-17 09:07:22 -0800

    initial checkin

---
+++ src/trees/TreeException.java
@@ -0,0 +1,12 @@
+package trees;
+
+import java.io.IOException;
+
+public class TreeException extends FamilyTree
+{
+	public TreeException(String s) throws IOException
+	{
+		// Pass s into a superclass ctor.
+		super(s);
+	}
+}
